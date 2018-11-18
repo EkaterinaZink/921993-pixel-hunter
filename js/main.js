af2efebbe79c2af021da469b45fda9ac86f9e824
EkaterinaESZ <ekaterina.zink@gmail.com>
2018-11-18 15:25:36 -0200

    испарвление ошибок

--- js/main.js
+++ js/main.js
@@ -1,37 +1,18 @@
 'use strict';
 
-let sсreens = document.querySelectorAll(`template`);
-// [`intro`, `greeting`, `rules`, `game-1`, `game-2`, `game-4`, `stats`, `modal-error`, `modal-confirm`];
+const sсreens = document.querySelectorAll(`template`);
 const Keys = {
   LEFT: 37,
   RIGHT: 39
 };
-/*
-const wrap = (it) => {
-  const shadow = document.createElement(`div`);
-  const content = it.content.cloneNode(true);
-  shadow.appendChild(content);
-  return shadow.cloneNode(true);
-};*/
 
 const main = document.getElementById(`main`);
-// const templates = sсreens.map(template);
 let screen = 0;
 
 const renderScreen = (item) => {
   main.innerHTML = ``;
-  main.appendChild(sсreens(item).content.cloneNode(true));
+  main.appendChild(sсreens[item].content.cloneNode(true));
 };
-
-const keyHandler = (evt) => {
-  if (evt.keyCode === Keys.LEFT) {
-    previousScreen();
-  }
-  if (evt.keyCode === Keys.RIGHT) {
-    nextScreen();
-  }
-};
-document.addEventListener(`keydown`, keyHandler);
 
 const nextScreen = () => {
   if (screen === sсreens.length - 1) {
@@ -47,17 +28,15 @@ const previousScreen = () => {
   renderScreen(--screen);
 };
 
-/* const arrows = document.createElement(`div`);
-arrows.innerHTML = `
-`;
-
-const createScreen = (evt) => {
-  const div = document.createElement(`div`);
-  div.innerHTML = evt;
-  return div;
+const keyHandler = (evt) => {
+  if (evt.keyCode === Keys.LEFT) {
+    previousScreen();
+  }
+  if (evt.keyCode === Keys.RIGHT) {
+    nextScreen();
+  }
 };
-*/
-// document.body.appendChild(createScreen(arrows));
+document.addEventListener(`keydown`, keyHandler);
 const moveLeft = document.getElementById(`move_left`);
 const moveRight = document.getElementById(`move_right`);
 moveLeft.addEventListener(`click`, previousScreen);
