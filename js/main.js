ee335f503238c3eb7503d80718bc352b00c6bf49
EkaterinaESZ <ekaterina.zink@gmail.com>
2018-11-25 17:20:05 -0200

    задание 2-1

--- js/main.js
+++ js/main.js
@@ -1,45 +1,5 @@
-'use strict';
 
-const sсreens = document.querySelectorAll(`template`);
-const Keys = {
-  LEFT: 37,
-  RIGHT: 39
-};
+import {renderScreen} from './utilits.js';
+import {introScreen} from './intro.js';
 
-const main = document.getElementById(`main`);
-let screen = 0;
-
-const renderScreen = (item) => {
-  main.innerHTML = ``;
-  main.appendChild(sсreens[item].content.cloneNode(true));
-};
-
-const nextScreen = () => {
-  if (screen === sсreens.length - 1) {
-    return;
-  }
-  renderScreen(++screen);
-};
-
-const previousScreen = () => {
-  if (screen === 0) {
-    return;
-  }
-  renderScreen(--screen);
-};
-
-const keyHandler = (evt) => {
-  if (evt.keyCode === Keys.LEFT) {
-    previousScreen();
-  }
-  if (evt.keyCode === Keys.RIGHT) {
-    nextScreen();
-  }
-};
-document.addEventListener(`keydown`, keyHandler);
-const moveLeft = document.getElementById(`move_left`);
-const moveRight = document.getElementById(`move_right`);
-moveLeft.addEventListener(`click`, previousScreen);
-moveRight.addEventListener(`click`, nextScreen);
-
-renderScreen(screen);
+renderScreen(introScreen);
