9342dd2f507f80aa91d88d8ebf468a6055f53966
EkaterinaESZ <ekaterina.zink@gmail.com>
2018-12-22 14:28:07 -0200

    pictures

--- js/backend-data.js
+++ js/backend-data.js
@@ -1,6 +1,6 @@
 
 const DEFAULT_NAME = `ANONYMOUS`;
 const SERVER_URL = `https://es.dump.academy/pixel-hunter`;
-const APP_ID = `536199`;
+const APP_ID = `921993`;
 
 export {DEFAULT_NAME, SERVER_URL, APP_ID};
